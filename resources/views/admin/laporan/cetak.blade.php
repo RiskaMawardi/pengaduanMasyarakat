b044db773b2713aa43adbf5a3bd4921da9f3e839
RiskaMawardi <riskamawardi439@gmail.com>
2023-03-29 22:36:45 +0700

    push

--- resources/views/admin/laporan/cetak.blade.php
+++ resources/views/admin/laporan/cetak.blade.php
@@ -2,6 +2,12 @@
 <html lang="en">
 
 <head>
+
+<div class="flex item-center">
+
+    <center>
+  <h2>PENGADUAN MASYARAKAT</h2>
+  </center>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +15,7 @@
 </head>
 
 <body onload="window.print()">
-    <table border="1px">
+<table border="1" cellpadding="3" cellspacing="0">
         <tr>
             <th>No</th>
             <th>NIK</th>
@@ -28,7 +34,7 @@
             <td>{{$i++}}</td>
             <td>{{$data->nik}}</td>
             <td>{{$data->isi_laporan}}</td>
-            <td><img src="/images/{{$data->foto}}" alt="" class="" style="width:10px;"></td>
+            <td><img src="/images/{{$data->foto}}" alt="" class="" style="width:50px;"></td>
             <td>-</td>
             <td>-</td>
             <td>{{$data->created_at}}</td>
